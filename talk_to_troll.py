9378a63c334d8051b89fe1bf68bb3e73edfe19dc
Mikkel Sannes Nylend <mikkelsn@stud.ntnu.no>
2016-04-13 10:07:06 +0200

    Speak should be replaced by talk_to_troll

---
+++ talk_to_troll.py
@@ -0,0 +1,29 @@
+#!/usr/bin/ev python
+
+import rospy
+import random
+from trollnode.msg import Expression
+
+def talker(speech, expression):
+	pub = rospy.Publisher('speaker', String, queue_size=10)
+	rospy.init_node('talker', anonymous=True)
+	#TODO fix .msg!!
+	expr_msg = Expression()
+	expr_msg.expression = expression
+	expr_msg.speech = speech
+	pub.publish(expr_msg)
+
+def talk_random_expression(speech):
+	expr = ["happy", "angry", "smile", "sad", "disgust", "surprise", "fear", "suspicios",
+		"blink", "pain", "duckface"]
+	talker(speech, expr[random.randint(0, len(expr)-1)
+
+def talk_default_exxpression(speech):
+	talker(speech, "happy")
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
+
