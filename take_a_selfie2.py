7d1dd80037fb4029727680b62a3fc8cbc08fefcd
hannemtr <hanne.marie@lifi.no>
2016-04-20 10:11:44 +0200

    Create take_a_selfie2.py

---
+++ take_a_selfie2.py
@@ -0,0 +1,173 @@
+import pyttsx
+import pygame
+import pygame.camera
+import pygame.image
+import speech_recognition as sr
+from PIL import Image
+import threading
+import time
+#import facebookupload
+
+import rospy
+import random
+
+#from std_msgs.msg import String
+from trollnode.msg import Expression
+import sys
+
+answer_given = False
+def takePicture(filename):
+    pygame.init()
+    pygame.camera.init()
+    cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+    cam.start()
+    img = cam.get_image()
+    pygame.image.save(img, filename + '.bmp')
+    cam.stop()
+    pygame.camera.quit()
+    pygame.quit()
+    return filename + '.bmp'
+
+def writeToFile(question, expression):
+    file = open("TalkFile.txt", "w")
+    file.write(question+"\n")
+    file.write(expression)
+    file.close()
+
+def speak(question, expression):
+    talker(question, expression)
+    #package = 'beginner_tutorials' 
+    #executable = 'talk_to_troll.py' 
+    #node = roslaunch.core.Node(package, executable, args="_param:="+str(question))
+
+    #launch = roslaunch.scriptapi.ROSLaunch() 
+    #launch = subprocess.call(["rosrun", package, executable])
+    #launch.start()
+
+    #process = launch.launch(node) 
+    #print process.is_alive()
+    #process.stop()
+
+    #print(question)
+    #engine = pyttsx.init()
+    #rate = engine.getProperty('rate')
+    #engine.setProperty('rate', rate + 0.5)
+    #engine.say(question)
+    #engine.runAndWait()
+
+def obtain_audio(r, m):
+    with m as source:
+        r.adjust_for_ambient_noise(source)
+
+def check_for_yes_or_no(r, audio):
+    global answer_given
+    try:
+        recognized = r.recognize_google(audio, show_all=True)
+        if len(recognized) == 0:
+            return None
+        alternatives = recognized['alternative']
+        if len(alternatives) == 0:
+            return None
+        for alternative in alternatives:
+            if alternative['transcript'] == 'yes':
+                answer_given = True
+                speak('Great, get behind the camera', "happy")
+                for _ in range(5): time.sleep(1)
+                speak('3', "smile")
+                time.sleep(0.5)
+                speak('2', "smile")
+                time.sleep(0.5)
+                speak('1', "smile")
+                time.sleep(0.5)
+                speak('Taking selfie...', talk_random_expression())
+                img = takePicture("picture")
+                speak('Thank you, I have uploaded it to my facebook profile', "blink")
+                return True
+            if alternative['transcript'] == 'no':
+                answer_given = True
+                speak('No selfie taken...', "sad")
+                return False
+    except sr.UnknownValueError:
+        return None
+    except sr.RequestError as e:
+        return None
+
+
+def startListening(question, expression, r, michrophone):
+    audio = obtain_audio(r, michrophone)
+    stop_listening = r.listen_in_background(michrophone, check_for_yes_or_no)
+    
+    try:
+        thread1 = threading.Thread(target = speak, args=(question, expression))
+        thread1.start()
+    except:
+        print "failed"
+        speak(question, expression)
+
+    for _ in range(200):
+        if answer_given: break;
+        time.sleep(0.1)
+    answer = stop_listening()
+    print answer
+    return answer
+
+pub = ""
+def talker(speech, express):
+    if express == "": express = talk_random_expression()
+    #speak(speech, express)
+
+    expr_msg = Expression()
+    expr_msg.expression = express
+    expr_msg.speech = speech
+
+    rospy.loginfo(speech +", "+express)
+
+    pub.publish(expr_msg)
+    #pub.publish(speech +", "+express)
+
+
+def talk_random_expression():
+    expr = ["happy", "angry", "smile", "sad", "disgust", "surprise", "fear", "suspicios",
+        "blink", "pain", "duckface"]
+    #talker(speech, expr[random.randint(0, len(expr)-1)])
+    return random.choice(expr)
+
+#def talk_default_expression(speech):
+#   talker(speech, "happy")
+
+
+def createPub():
+    try:
+        global pub
+        #pub = rospy.Publisher('chatter', String, queue_size=10)
+        pub = rospy.Publisher('trollExpression', Expression, queue_size=10)
+        rospy.init_node('talker', anonymous=True)
+    
+        #talker(0)#rospy.myargv(argv=sys.argv)[0])
+    except rospy.ROSInterruptException:
+        pass
+
+
+def main():
+
+    r = sr.Recognizer()
+    michrophone = sr.Microphone()
+    question = 'Would you like to take a selfie with me?'
+    expression = "smile"
+    createPub()
+
+
+    answer = startListening(question, expression, r, michrophone)
+    
+
+    timesAsked = 1
+    while not answer_given and timesAsked < 4:
+        timesAsked += 1  
+        question = 'I did not hear you, please repeat with yes or no.'
+        expression = "surprise"
+
+        answer = startListening(question, expression, r, michrophone)
+
+
+main()
+#img = takePicture("picture")
